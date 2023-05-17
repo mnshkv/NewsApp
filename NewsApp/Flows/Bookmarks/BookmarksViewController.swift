5e7ed06c6c3e7d681a6293e8d72eb051d9a62778
DmKeller <dmit.keller@gmail.com>
2023-05-17 12:50:05 +0300

    UI for Bokmarks

--- NewsApp/Flows/Bookmarks/BookmarksViewController.swift
+++ NewsApp/Flows/Bookmarks/BookmarksViewController.swift
@@ -11,25 +11,68 @@ import SnapKit
 class BookmarksViewController: UIViewController {
     
     private let bookmarkService = BookmarkService()
-
+    
+    // MARK: - Outlets
+    
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Saved articles to the library"
+        label.font = UIFont(name: "Inter", size: 16)
+        label.textColor = UIColor(red: 124/255, green: 130/255, blue: 161/255, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "boommarksCell")
+        tableView.dataSource = self
+        return tableView
+    } ()
+    
+    
+    
+    // MARK:- LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupHierarchy()
+        setupLayout()
+        
         // Do any additional setup after loading the view.
         bookmarkService.fetchBookmarks { bookmarks in
             print("APP: ", bookmarks)
         }
+        
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboar dSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+        view.addSubview(subtitleLabel)
     }
-    */
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.right.equalTo(view.snp.right)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.left.equalTo(view.snp.left)
+        }
+        subtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.leading.equalTo(view.snp.leading).offset(16)
+            make.trailing.equalTo(view.snp.trailing).offset(-16)
+        }
+    }
+}
 
+extension BookmarksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "boommarksCell", for: indexPath)
+        return cell
+    }
 }
---
+++ NewsApp/Flows/Bookmarks/Views/BookmarkTableViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  BookmarkTableViewCell.swift
+//  NewsApp
+//
+//  Created by Келлер Дмитрий on 17.05.2023.
+//
+
+import UIKit
+
+class BookmarkTableViewCell: UITableViewCell {
+    
+    
+    // MARK: - Properties
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let nameBookmarkLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let detailLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
