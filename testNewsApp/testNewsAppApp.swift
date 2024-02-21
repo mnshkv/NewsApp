246daa4fd8550db8c58cdb5e295276afac3d865c
a.g.menshikov <a.g.menshikov@tinkoff.ru>
2024-02-21 16:11:14 +0300

    update

---
+++ testNewsApp/testNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testNewsAppApp.swift
+//  testNewsApp
+//
+//  Created by Aleksandr Menshikov on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct testNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
